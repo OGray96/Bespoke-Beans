6884587a5a180ab084c759b79b93219d1b7fc071
OGray96 <ogray54@gmail.com>
2022-10-18 20:54:45 +1100

    updating folder structure

---
+++ src/components/pages/homepage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+export default function Home() {
+    return (
+      <div>
+        
+        <h1>
+            Website Coming Soon!
+        </h1>
+
+      </div>
+    );
+}
