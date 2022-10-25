69308657deec7a087690a79721c6f77ce1adeda5
Zoki91 <zmitrevski2@gmail.com>
2022-10-25 20:53:49 +1100

    info page done

--- client/src/pages/Homepage.js
+++ client/src/pages/Homepage.js
@@ -17,9 +17,24 @@ export default function Home() {
             <div className="about-us">
               <div className="about-us-wwd">
                 <p>
-                  Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                  Nesciunt inventore repellat optio quia magnam repellendus
-                  eligendi facere deserunt nisi neque?
+                  Bespoke Beans is a small coffee shop in the heart of
+                  Melbourne, serving only the finest, most expensive coffees in
+                  town. But don't let that fool you—we know our stuff! We're
+                  obsessed with finding the perfect bean and putting it in your
+                  cup. Our name was inspired by two different things: the first
+                  is our love for fine clothing and accessories; the second is
+                  our belief that good coffee should be treated like an art
+                  form, not a commodity. Every cup we serve is hand-crafted to
+                  ensure that it meets our standards of quality, which are high.
+                  Oliver, one of our baristas, has been with Bespoke Beans since
+                  we opened our doors in 2022. He's passionate about coffee and
+                  is always willing to help new customers find their perfect
+                  cup. He takes pride in his work and wants everyone who walks
+                  through our doors to feel welcome and comfortable. Zoran is
+                  another one of our baristas who has been serving up cups of
+                  hot joe at Bespoke Beans since 2022. His favourite part about
+                  working here? Learning about different blends from all over
+                  the world!
                 </p>
               </div>
             </div>
