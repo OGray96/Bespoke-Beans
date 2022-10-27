9d6736a4c0f55bdc9276b2d75a08cc0e52e23882
OGray96 <ogray54@gmail.com>
2022-10-27 19:14:37 +1100

    Added conditional formatting to Navbar

---
+++ client/src/utils/queries.js
@@ -0,0 +1,56 @@
+import { gql } from '@apollo/client';
+
+export const QUERY_PRODUCTS = gql`
+  query getProducts($category: ID) {
+    products(category: $category) {
+      _id
+      name
+      description
+      price
+      stock
+    }
+  }
+`;
+
+
+
+export const QUERY_CHECKOUT = gql`
+  query getCheckout($products: [ID]!) {
+    checkout(products: $products) {
+      session
+    }
+  }
+`;
+
+export const QUERY_ALL_PRODUCTS = gql`
+  {
+    products {
+      _id
+      name
+      description
+      price
+      stock
+    }
+  }
+`;
+
+
+export const QUERY_USER = gql`
+  {
+    user {
+      firstName
+      lastName
+      orders {
+        _id
+        date
+        products {
+          _id
+          name
+          description
+          price
+          stock
+        }
+      }
+    }
+  }
+`;
