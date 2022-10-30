2d06783c46263b83e45f08bca0715331b363bc66
Zoki91 <zmitrevski2@gmail.com>
2022-10-30 21:57:00 +1100

    cart page completed with styling

--- client/src/pages/Orders/Order.js
+++ client/src/pages/Orders/Order.js
@@ -1,45 +1,80 @@
 import { CartContext } from "../../context/CartContext";
 import React, { useContext, useEffect } from "react";
-import {QUERY_ALL_PRODUCTS} from '../../utils/queries'
+import { QUERY_ALL_PRODUCTS } from "../../utils/queries";
 import { useQuery } from "@apollo/client";
-import './Order.css'
+import "./Order.css";
 
-function Order(){
-    const cart = useContext(CartContext);
-    const { loading, data} = useQuery(QUERY_ALL_PRODUCTS);
-    const items = data?.products || []
-    const productQuantity = cart.getProductQuantity(items.id)
-    console.log(cart.items)
-    console.log(productQuantity)
-    
+function Order() {
+  const cart = useContext(CartContext);
+  const { loading, data } = useQuery(QUERY_ALL_PRODUCTS);
+  const items = data?.products || [];
+  const productQuantity = cart.getProductQuantity(items.id);
+  console.log(cart.items);
+  console.log(productQuantity);
 
-    if(loading){
-        return(
-            <>
-                <h3>LOADING...</h3>
-            </>
-         )} else {
-            return (
-                <>
-                <div>
-                    {cart.items.map((item) => (
-                       <div className="coffee-order-container">
-                            <h2>Coffee Name: {item.name}</h2>
-                            <h2>Quantity: {item.quantity}</h2>
-                            <h2>Price per Coffee : ${item.price * item.quantity}</h2>
-                            <h2>Price : ${item.price * item.quantity}</h2>
-                            <button onClick={() => cart.addOnetoCart(item._id, item.name, item.price)}> + </button>
-                            <button onClick={() => cart.removeOneFromCart(item._id)}> - </button>
-                       </div> 
-                    ))}
+  if (loading) {
+    return (
+      <>
+        <h3>LOADING...</h3>
+      </>
+    );
+  } else {
+    return (
+      <>
+        <div className="coffee-order-container cart-card">
+          <span className="shopping">Shopping Cart</span>
+          {cart.items.map((item) => (
+            <div className="cart-items">
+              <span className="coffee-name-span">☕ {item.name}</span>
 
-                </div>
-                <h3>Total Coffees: {cart.items.reduce((accum,item) => accum + item.quantity, 0)}</h3>
-                <h3>Total Price: ${cart.items.reduce((accum,item) => accum + (item.quantity * item.price), 0)}</h3>
-                <button className="btn-black">Checkout</button>
-            </>
-            )
-         }
+              <div className="quantity">
+                <span className="coffee-quantity-span">✏️ {item.quantity}</span>
+                <button
+                  className="up-down-btn"
+                  onClick={() =>
+                    cart.addOnetoCart(item._id, item.name, item.price)
+                  }
+                >
+                  {" "}
+                  +{" "}
+                </button>
+                <button
+                  className="up-down-btn"
+                  onClick={() => cart.removeOneFromCart(item._id)}
+                >
+                  {" "}
+                  -{" "}
+                </button>
+              </div>
+              <div className="price">
+                {/* <h3 className="coffee-price">Price per Coffee : $</h3> */}
+                {/* <span className="coffee-price"> {item.price * item.quantity}</span> */}
+                <h3>🧾${item.price * item.quantity}</h3>
+              </div>
+
+              <br></br>
+            </div>
+          ))}
+          <div className="total-coffes">
+            <h3>
+              Total Coffees:{" "}
+              {cart.items.reduce((accum, item) => accum + item.quantity, 0)}
+            </h3>
+            <h3>
+              Total Price: $
+              {cart.items.reduce(
+                (accum, item) => accum + item.quantity * item.price,
+                0
+              )}
+            </h3>
+          </div>
+          <div className="checkout-btn">
+            <button className="cart-btn">Checkout</button>
+          </div>
+        </div>
+      </>
+    );
+  }
 }
 
 export default Order;
