7e75c3d7f27d6e7afb32306b06a9489a8708dd5c
OGray96 <ogray54@gmail.com>
2022-10-31 12:24:20 +1100

    Conditional user pathways

--- client/src/pages/Orders/Order.js
+++ client/src/pages/Orders/Order.js
@@ -3,6 +3,7 @@ import React, { useContext, useEffect } from "react";
 import { QUERY_ALL_PRODUCTS } from "../../utils/queries";
 import { useQuery } from "@apollo/client";
 import "./Order.css";
+import Auth from "../../utils/auth";
 
 function Order() {
   const cart = useContext(CartContext);
@@ -11,81 +12,91 @@ function Order() {
   const productQuantity = cart.getProductQuantity(items.id);
   console.log(cart.items);
   console.log(productQuantity);
-
-  if (loading) {
-    return (
-      <>
-        <h3>LOADING...</h3>
-      </>
-    );
-  } else if ((cart.items.reduce((accum, item) => accum + item.quantity, 0)) < 1){
-    return (
-        <div>
-            <div className="empty-cart">
-                <h3 className="empty-cart-text">Looks like you cart is empty!</h3>
+  if(Auth.loggedIn()){
+    if (loading) {
+        return (
+          <>
+            <h3>LOADING...</h3>
+          </>
+        );
+      } else if ((cart.items.reduce((accum, item) => accum + item.quantity, 0)) < 1){
+        return (
+            <div>
+                <div className="empty-cart">
+                    <h3 className="empty-cart-text">Looks like you cart is empty!</h3>
+                </div>
             </div>
-        </div>
-
-        
-    )
+    
+            
+        )
+      } else {
+        return (
+          <>
+            <div className="coffee-order-container cart-card">
+              <span className="shopping">Shopping Cart</span>
+              {cart.items.map((item) => (
+                <div className="cart-items">
+                  <span className="coffee-name-span">☕ {item.name}</span>
+    
+                  <div className="quantity">
+                    <span className="coffee-quantity-span">✏️ {item.quantity}</span>
+                    <button
+                      className="up-down-btn"
+                      onClick={() =>
+                        cart.addOnetoCart(item._id, item.name, item.price)
+                      }
+                    >
+                      {" "}
+                      +{" "}
+                    </button>
+                    <button
+                      className="up-down-btn"
+                      onClick={() => cart.removeOneFromCart(item._id)}
+                    >
+                      {" "}
+                      -{" "}
+                    </button>
+                  </div>
+                  <div className="price">
+                    {/* <h3 className="coffee-price">Price per Coffee : $</h3> */}
+                    {/* <span className="coffee-price"> {item.price * item.quantity}</span> */}
+                    <h3>🧾${item.price * item.quantity}</h3>
+                    <button className="cart-btn" onClick={() => cart.deleteFromCart(item._id)}>Remove</button>
+                  </div>
+    
+                  <br></br>
+                </div>
+              ))}
+              <div className="total-coffees">
+                <h3>
+                  Total Coffees:{" "}
+                  {cart.items.reduce((accum, item) => accum + item.quantity, 0)}
+                </h3>
+                <h3>
+                  Total Price: $
+                  {cart.items.reduce(
+                    (accum, item) => accum + item.quantity * item.price,
+                    0
+                  )}
+                </h3>
+              </div>
+              <div className="checkout-btn">
+                <button className="cart-btn">Checkout</button>
+              </div>
+            </div>
+          </>
+        );
+      }
   } else {
     return (
-      <>
-        <div className="coffee-order-container cart-card">
-          <span className="shopping">Shopping Cart</span>
-          {cart.items.map((item) => (
-            <div className="cart-items">
-              <span className="coffee-name-span">☕ {item.name}</span>
-
-              <div className="quantity">
-                <span className="coffee-quantity-span">✏️ {item.quantity}</span>
-                <button
-                  className="up-down-btn"
-                  onClick={() =>
-                    cart.addOnetoCart(item._id, item.name, item.price)
-                  }
-                >
-                  {" "}
-                  +{" "}
-                </button>
-                <button
-                  className="up-down-btn"
-                  onClick={() => cart.removeOneFromCart(item._id)}
-                >
-                  {" "}
-                  -{" "}
-                </button>
-              </div>
-              <div className="price">
-                {/* <h3 className="coffee-price">Price per Coffee : $</h3> */}
-                {/* <span className="coffee-price"> {item.price * item.quantity}</span> */}
-                <h3>🧾${item.price * item.quantity}</h3>
-                <button className="cart-btn" onClick={() => cart.deleteFromCart(item._id)}>Remove</button>
-              </div>
-
-              <br></br>
-            </div>
-          ))}
-          <div className="total-coffees">
-            <h3>
-              Total Coffees:{" "}
-              {cart.items.reduce((accum, item) => accum + item.quantity, 0)}
-            </h3>
-            <h3>
-              Total Price: $
-              {cart.items.reduce(
-                (accum, item) => accum + item.quantity * item.price,
-                0
-              )}
-            </h3>
-          </div>
-          <div className="checkout-btn">
-            <button className="cart-btn">Checkout</button>
-          </div>
+        <div>
+        <div className="empty-cart">
+            <h3 className="empty-cart-text">Looks like you need to login before you can look at your cart!</h3>
         </div>
-      </>
-    );
+    </div>  
+    )
   }
+
 }
 
 export default Order;
--- client/src/pages/Products/Products.js
+++ client/src/pages/Products/Products.js
@@ -7,6 +7,8 @@ import { QUERY_ALL_PRODUCTS } from "../../utils/queries";
 import { useQuery } from "@apollo/client";
 import { CartContext } from "../../context/CartContext";
 
+import Auth from "../../utils/auth";
+
 
 
 import coffee1 from "../../images/coffee1.png";
@@ -25,88 +27,172 @@ export default function Products(props) {
   const cart = useContext(CartContext);
   const productQuantity = cart.getProductQuantity(items.id)
   console.log(productQuantity)
-  return (
-    <div className="products">
-      {/* TESTING */}
-      {/* <div
-        style={{
-          position: 'fixed',
-          right: '3rem',
-          display: 'flex',
-          flexDirection: 'column',
-        }}
-      >
-        <button onClick={() => console.log(cart.items)}>Show Cart</button>
-        <button onClick={() => console.log(cart.getTotal())}>Cart Total</button>
-        <button onClick={() => cart.removeAll(1)}>Remove All</button>
-        <button onClick={() => cart.removeOne(1)}>Remove One</button>
-      </div> */}
-      {/* TESTING */}
-
-      <div className="products-container">
-        <div className="products-header">
-          <div className="products-header-content">
-            <h1>
-              Please select from one of our Bespoke Coffee's today and we'll
-              make sure it's ready for you to pick up before your boss knows you
-              were even gone!
-            </h1>
+  if (Auth.loggedIn()) {
+    return (
+      <div className="products">
+        {/* TESTING */}
+        {/* <div
+          style={{
+            position: 'fixed',
+            right: '3rem',
+            display: 'flex',
+            flexDirection: 'column',
+          }}
+        >
+          <button onClick={() => console.log(cart.items)}>Show Cart</button>
+          <button onClick={() => console.log(cart.getTotal())}>Cart Total</button>
+          <button onClick={() => cart.removeAll(1)}>Remove All</button>
+          <button onClick={() => cart.removeOne(1)}>Remove One</button>
+        </div> */}
+        {/* TESTING */}
+  
+        <div className="products-container">
+          <div className="products-header">
+            <div className="products-header-content">
+              <h1>
+                Please select from one of our Bespoke Coffee's today and we'll
+                make sure it's ready for you to pick up before your boss knows you
+                were even gone!
+              </h1>
+            </div>
           </div>
-        </div>
-
-        <div className="products-main">
-          <div className="products-items">
-            {items.map((item, index) => (
-              <div
-                className={"products-item " + (index % 2 ? "dark" : "light")}
-                key={index}
-              >
-                <div className="products-item-content">
-                  <div className="products-item-content-container">
-                    <div className="products-item-title">
-                      <h2>{item.name}</h2>
-                    </div>
-                    <div className="products-item-description">
-                      <p>{item.description}</p>
-                    </div>
-                    <div className="products-item-price">
-                      <span>{item.price}</span>
-                    </div>
-                    <div className="products-item-cart-btn">
-
-                      <button onClick={() => cart.addOnetoCart(item._id, item.name, item.price)} className={index % 2 ? "btn-white" : "btn-black"}>Add to Cart</button>
+  
+          <div className="products-main">
+            <div className="products-items">
+              {items.map((item, index) => (
+                <div
+                  className={"products-item " + (index % 2 ? "dark" : "light")}
+                  key={index}
+                >
+                  <div className="products-item-content">
+                    <div className="products-item-content-container">
+                      <div className="products-item-title">
+                        <h2>{item.name}</h2>
+                      </div>
+                      <div className="products-item-description">
+                        <p>{item.description}</p>
+                      </div>
+                      <div className="products-item-price">
+                        <span>{item.price}</span>
+                      </div>
+                      <div className="products-item-cart-btn">
+  
+                        <button onClick={() => cart.addOnetoCart(item._id, item.name, item.price)} className={index % 2 ? "btn-white" : "btn-black"}>Add to Cart</button>
+                      </div>
                     </div>
                   </div>
-                </div>
-
-                <div className="products-item-image">
-                  <motion.img
-                    initial="offscreen"
-                    whileInView="onscreen"
-                    viewport={{ once: true }}
-                    variants={{
-                      offscreen: {
-                        y: 200,
-                      },
-                      onscreen: {
-                        y: 0,
-                        rotate: -10,
-                        transition: {
-                          type: "spring",
-                          bounce: 0.4,
-                          duration: 0.8,
+  
+                  <div className="products-item-image">
+                    <motion.img
+                      initial="offscreen"
+                      whileInView="onscreen"
+                      viewport={{ once: true }}
+                      variants={{
+                        offscreen: {
+                          y: 200,
                         },
-                      },
-                    }}
-                    src={imgArray[index]}
-                    alt="product-placeholder.png"
-                  />
+                        onscreen: {
+                          y: 0,
+                          rotate: -10,
+                          transition: {
+                            type: "spring",
+                            bounce: 0.4,
+                            duration: 0.8,
+                          },
+                        },
+                      }}
+                      src={imgArray[index]}
+                      alt="product-placeholder.png"
+                    />
+                  </div>
                 </div>
-              </div>
-            ))}
+              ))}
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    return (
+      <div className="products">
+        {/* TESTING */}
+        {/* <div
+          style={{
+            position: 'fixed',
+            right: '3rem',
+            display: 'flex',
+            flexDirection: 'column',
+          }}
+        >
+          <button onClick={() => console.log(cart.items)}>Show Cart</button>
+          <button onClick={() => console.log(cart.getTotal())}>Cart Total</button>
+          <button onClick={() => cart.removeAll(1)}>Remove All</button>
+          <button onClick={() => cart.removeOne(1)}>Remove One</button>
+        </div> */}
+        {/* TESTING */}
+  
+        <div className="products-container">
+          <div className="products-header">
+            <div className="products-header-content">
+              <h1>
+                Please select from one of our Bespoke Coffee's today and we'll
+                make sure it's ready for you to pick up before your boss knows you
+                were even gone!
+              </h1>
+            </div>
+          </div>
+  
+          <div className="products-main">
+            <div className="products-items">
+              {items.map((item, index) => (
+                <div
+                  className={"products-item " + (index % 2 ? "dark" : "light")}
+                  key={index}
+                >
+                  <div className="products-item-content">
+                    <div className="products-item-content-container">
+                      <div className="products-item-title">
+                        <h2>{item.name}</h2>
+                      </div>
+                      <div className="products-item-description">
+                        <p>{item.description}</p>
+                      </div>
+                      <div className="products-item-price">
+                        <span>{item.price}</span>
+                      </div>
+                    </div>
+                  </div>
+  
+                  <div className="products-item-image">
+                    <motion.img
+                      initial="offscreen"
+                      whileInView="onscreen"
+                      viewport={{ once: true }}
+                      variants={{
+                        offscreen: {
+                          y: 200,
+                        },
+                        onscreen: {
+                          y: 0,
+                          rotate: -10,
+                          transition: {
+                            type: "spring",
+                            bounce: 0.4,
+                            duration: 0.8,
+                          },
+                        },
+                      }}
+                      src={imgArray[index]}
+                      alt="product-placeholder.png"
+                    />
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+  
 }
