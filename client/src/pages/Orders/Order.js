b6461aea191969ff738b3a4dcf9a82c4d53fdd7d
OGray96 <ogray54@gmail.com>
2022-10-31 16:48:03 +1100

    Update stripe fetch request version 3

--- client/src/pages/Orders/Order.js
+++ client/src/pages/Orders/Order.js
@@ -13,7 +13,7 @@ function Order() {
   console.log(cart)
 
   const checkout = async () => {
-    await fetch('https://frozen-mesa-21808.herokuapp.com/checkout', {
+    await fetch('/checkout', {
         method: "POST",
         headers: {
             'Content-Type' : 'application/json'
